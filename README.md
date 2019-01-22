548d935faf85829f140099f3f134b95721946c3c
Arul John <arulsjohn@gmail.com>
2019-01-22 17:20:19 -0500

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python script to connect to MySQL
+
+This is a simple Python script that connects to a local or remote MySQL server and displays the names of all tables in taht database.
+
+It uses pymysql library.
---
+++ pymysqltables.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+ 
+"""
+This script connects to a remote MySQL database
+and displays all table names
+Coded by Arul John
+Created on 22 January 2019
+Last Updated: <>
+"""
+
+import pymysql
+
+# Credentials
+auth = {'username': '****', 'password': '****', 'port': 3306, \
+        'database': '****', 'host': 'localhost'}
+
+# Connect to MySQL
+db = pymysql.connect(user=auth['username'], password=auth['password'], \
+                    database=auth['database'], host=auth['host'], port=auth['port'])
+cursor = db.cursor()
+sql = 'show tables'
+cursor.execute(sql)
+try:
+    cursor.execute(sql)
+    # Display all table names
+    print('LIST OF TABLES')
+    for (table,) in cursor:
+        print(table), 
+except Exception as e:
+    print('ERROR: {}'.format(repr(e)))
+
+# Close database connection
+cursor.close()
+db.close()
